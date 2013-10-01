27cadaa86addaa5b696afa8e2bcd397d3e7a96ed
jcfr <jcfr@3bd1e089-480b-0410-8dfb-8563597acbee>
2013-10-01 19:25:00 +0000

    ENH: Improve readability using vtkNew where possible
    In addition to be shorter, it simply conveys the idea of locally
    instantiating an object.
    
    Indeed, vtkNew allow to instantiate an object on the heap and deleting it
    when it goes out of scope if its reference count has been incremented by
    either assigning the object to a smart pointer or calling Register.
    
    git-svn-id: http://svn.slicer.org/Slicer4/trunk@22539 3bd1e089-480b-0410-8dfb-8563597acbee

--- Modules/Loadable/Markups/MRMLDM/vtkMRMLMarkupsDisplayableManager2D.cxx
+++ Modules/Loadable/Markups/MRMLDM/vtkMRMLMarkupsDisplayableManager2D.cxx
@@ -56,7 +56,6 @@
 #include <vtkRenderWindowInteractor.h>
 #include <vtkSeedRepresentation.h>
 #include <vtkSeedWidget.h>
-#include <vtkSmartPointer.h>
 #include <vtkWeakPointer.h>
 #include <vtkWidgetRepresentation.h>
 
@@ -1512,10 +1511,10 @@ void vtkMRMLMarkupsDisplayableManager2D::GetWorldToLocalCoordinates(vtkMRMLMarku
   vtkMRMLTransformNode* tnode = node->GetParentTransformNode();
   if (tnode != NULL && tnode->IsLinear())
     {
-    vtkSmartPointer<vtkMatrix4x4> transformToWorld = vtkSmartPointer<vtkMatrix4x4>::New();
+    vtkNew<vtkMatrix4x4> transformToWorld;
     transformToWorld->Identity();
     vtkMRMLLinearTransformNode *lnode = vtkMRMLLinearTransformNode::SafeDownCast(tnode);
-    lnode->GetMatrixTransformToWorld(transformToWorld);
+    lnode->GetMatrixTransformToWorld(transformToWorld.GetPointer());
     transformToWorld->Invert();
 
     double p[4];
